3d36f851fa7ba04de8ed5129c7c44e6c1e3f352f
kizyma <kizimayarik01@gmail.com>
2021-07-14 15:21:50 +0300

    Further changes to models, added DB population script

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -31,20 +31,22 @@ def populate():
         'url':'http://flask.pocoo.org'} 
         ]
 
-    categories = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    categories = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
 
 
     for cat, cat_data in categories.items():
-        c = add_cat(cat)
+        cat_views = cat_data['views']
+        cat_likes = cat_data['likes']
+        c = add_cat(cat, cat_views, cat_likes)
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
 # Print added categories
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
-            print(f'- {c}: {p}')
+            print(f'- {c}: {p}, Likes: {c.likes}, Views: {c.views}')
 
 def add_page(category, title, url, views=0):
     p = Page.objects.get_or_create(category=category, title=title)[0]
@@ -53,8 +55,8 @@ def add_page(category, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c 
 
